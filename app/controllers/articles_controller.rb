557f3967e3ca8478b5d5978c3c27360d80c55b1f
wuxsoft@gmail.com <wuxsoft@gmail.com>
2016-08-24 23:14:08 +0800

    移动查次数保存位置

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    # @article.record_timestamps = false
   end
 
   def destory
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :member
+  after_show :set_view_count
+
+  def set_view_count
+    self.update_column(:view_count, self.view_count + 1)
+  end
 end
