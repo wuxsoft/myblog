217847de15b2f24279d0ca223258961ff8fc425f
wuxsoft@gmail.com <wuxsoft@gmail.com>
2016-08-24 22:07:07 +0800

    添加显示页面，添加时间显示格式

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,11 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.order('id desc')
     # 已设置默认分页条数为 20 条，设置为 1 为了调试
-    @articles = @articles.paginate page: params[:page], per_page: 1
+    @articles = @articles.paginate page: params[:page] # , per_page: 1
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def destory
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,13 @@ module ApplicationHelper
     content_tag('ul', raw(items.join(EMPTY_STRING)), opts)
   end
 
+  def timeago(time, opts = {})
+    opts[:format] ||= :short
+    return raw(content_tag(:span, l(time, format: opts[:format]), title: time)) if 1.days.ago > time
+    ago = time_ago_in_words(time)
+    content_tag(:span, [ago, '前'].join(''), title: time)
+  end
+
   def notice_message
     flash_messages = []
     flash.each do |type, message|
