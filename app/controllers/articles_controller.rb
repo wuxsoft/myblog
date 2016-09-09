8701be40eeecb6f0f9273a5debab06d1deea656b
wuxsoft@gmail.com <wuxsoft@gmail.com>
2016-09-09 20:47:43 +0800

    修改样式及文章过滤

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.order('id desc')
     # 已设置默认分页条数为 20 条，设置为 1 为了调试
+    @articles = @articles.where(case_id: params[:case_id])
     @articles = @articles.paginate page: params[:page] # , per_page: 1
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,16 @@ class Article < ApplicationRecord
   belongs_to :member
   belongs_to :case
   has_many :replies
+  after_create :update_case_count
+  # after_update :update_case_count
   def set_view_count
     self.update_column(:view_count, self.view_count + 1)
+  end
+
+  def update_case_count
+    self.case.count += 1
+    # self.case.count = Article.where(case_id: self.case.id).count
+    self.case.save
+    true
   end
 end
